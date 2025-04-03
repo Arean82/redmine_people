5c4817a2d17bdaf60211bb4c13eb274699ce2ee7
arean82 <38061300+Arean82@users.noreply.github.com>
2025-04-03 12:25:17 +0530

    Update 001_append_user_fields.rb

--- db/migrate/001_append_user_fields.rb
+++ db/migrate/001_append_user_fields.rb
@@ -1,36 +1,20 @@
-class AppendUserFields < ActiveRecord::Migration
-  def self.up
-    add_column :users, :phone, :string
-    add_column :users, :address, :string
-    add_column :users, :skype, :string
-    add_column :users, :birthday, :date
-    add_column :users, :job_title, :string
-    add_column :users, :company, :string
-    add_column :users, :middlename, :string
-    add_column :users, :gender, :smallint
-    add_column :users, :twitter, :string
-    add_column :users, :facebook, :string
-    add_column :users, :linkedin, :string
-    add_column :users, :background, :text
-    add_column :users, :appearance_date, :date
-    add_column :users, :department_id, :integer
+class AppendUserFields < ActiveRecord::Migration[6.1]
+  def change
+    change_table :users, bulk: true do |t|
+      t.string  :phone
+      t.string  :address
+      t.string  :skype
+      t.date    :birthday
+      t.string  :job_title
+      t.string  :company
+      t.string  :middlename
+      t.integer :gender, limit: 2
+      t.string  :twitter
+      t.string  :facebook
+      t.string  :linkedin
+      t.text    :background
+      t.date    :appearance_date
+      t.integer :department_id, index: true
+    end
   end
-
-  def self.down
-    remove_column :users, :phone
-    remove_column :users, :address
-    remove_column :users, :skype
-    remove_column :users, :birthday
-    remove_column :users, :job_title
-    remove_column :users, :company
-    remove_column :users, :middlename
-    remove_column :users, :gender
-    remove_column :users, :twitter
-    remove_column :users, :facebook
-    remove_column :users, :linkedin
-    remove_column :users, :background
-    remove_column :users, :appearance_date
-    remove_column :users, :department_id
-  end
-
 end
