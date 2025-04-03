5101204f2430bad44cffde7b23591f135a96da43
arean82 <38061300+Arean82@users.noreply.github.com>
2025-04-03 12:18:13 +0530

    Update departments_helper.rb

--- app/helpers/departments_helper.rb
+++ app/helpers/departments_helper.rb
@@ -4,47 +4,43 @@ module DepartmentsHelper
   end
 
   def parent_department_select_tag(department)
-    selected = department.parent if department
-    # retrieve the requested parent department
-    parent_id = (params[:department] && params[:department][:parent_id]) || params[:parent_id]
-    if parent_id
-      selected = (parent_id.blank? ? nil : Department.find(parent_id))
-    end
+    selected = department&.parent
+
+    # Retrieve the requested parent department
+    parent_id = params.dig(:department, :parent_id) || params[:parent_id]
+    selected = Department.find_by(id: parent_id) if parent_id.present?
+
     departments = department ? department.allowed_parents.compact : Department.all
-    options = ''
-    options << "<option value=''></option>" 
-    options << department_tree_options_for_select(departments, :selected => selected)
-    content_tag('select', options.html_safe, :name => 'department[parent_id]', :id => 'department_parent_id')
+    options = content_tag(:option, '', value: '')
+
+    options << department_tree_options_for_select(departments, selected: selected)
+    select_tag 'department[parent_id]', options.html_safe, id: 'department_parent_id'
   end  
 
   def department_tree_options_for_select(departments, options = {})
-    s = ''
-    department_tree(departments) do |department, level|
-      name_prefix = (level > 0 ? '&nbsp;' * 2 * level + '&#187; ' : '').html_safe
-      tag_options = {:value => department.id}
-      if department == options[:selected] || (options[:selected].respond_to?(:include?) && options[:selected].include?(department))
-        tag_options[:selected] = 'selected'
-      else
-        tag_options[:selected] = nil
-      end
-      tag = options[:tag] || 'option'
+    safe_join(department_tree(departments).map do |department, level|
+      name_prefix = ('&nbsp;' * 2 * level + '&#187; ').html_safe if level.positive?
+      tag_options = { value: department.id, selected: department == options[:selected] }
       tag_options.merge!(yield(department)) if block_given?
-      s << content_tag(tag, name_prefix + h(department), tag_options)
-    end
-    s.html_safe
+
+      content_tag(options[:tag] || :option, name_prefix.to_s + h(department.name), tag_options)
+    end)
   end  
 
   def department_tree_links(departments, options = {})
-    s = ''
-    s << "<ul class='department-tree'>"
-    s << "<li> #{link_to l(:label_people_all), {}} </li>"
-    department_tree(departments) do |department, level| 
-      name_prefix = (level > 0 ? ('&nbsp;' * 2 * level + '&#187; ') : '')
-      s << "<li>" + name_prefix + link_to(department.name, {:department_id => department.id}, :class => "#{'selected' if @department && department == @department}")
-      s << "</li>"                          
-    end 
-    s << "</ul>"
-    s.html_safe   
-  end
+    content_tag(:ul, class: 'department-tree') do
+      safe_join([
+        content_tag(:li, link_to(l(:label_people_all), {})),
 
+        department_tree(departments).map do |department, level|
+          name_prefix = ('&nbsp;' * 2 * level + '&#187; ').html_safe if level.positive?
+          css_class = 'selected' if @department && department == @department
+
+          content_tag(:li) do
+            name_prefix.to_s + link_to(department.name, { department_id: department.id }, class: css_class)
+          end
+        end
+      ].flatten.compact)
+    end
+  end
 end
