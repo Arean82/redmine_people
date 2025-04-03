58b9e60781013cc7f005e551e8eec97d17120e2d
arean82 <38061300+Arean82@users.noreply.github.com>
2025-04-03 12:18:59 +0530

    Update people_helper.rb

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,26 +1,28 @@
 module PeopleHelper
-
   def birthday_date(person)
-    if person.birthday.day == Date.today.day && person.birthday.month == Date.today.month
-       "#{l(:label_today).capitalize} (#{person.age + 1})"
+    return unless person&.birthday
+
+    if person.birthday.today?
+      "#{l(:label_today).capitalize} (#{person.age.to_i + 1})"
     else
-      "#{person.birthday.day} #{t('date.month_names')[person.birthday.month]} (#{person.age + 1})"
+      "#{person.birthday.day} #{t('date.month_names')[person.birthday.month]} (#{person.age.to_i + 1})"
     end
   end
 
   def people_principals_check_box_tags(name, principals)
-    s = ''
-    principals.each do |principal|
-      s << "<label>#{ check_box_tag name, principal.id, false, :id => nil } #{principal.is_a?(Group) ? l(:label_group) + ': ' + principal.to_s : principal}</label>\n"
-    end
-    s.html_safe
+    safe_join(principals.map do |principal|
+      label_tag(nil) do
+        check_box_tag(name, principal.id, false, id: nil) +
+          " #{principal.is_a?(Group) ? l(:label_group) + ': ' + principal.to_s : principal}"
+      end
+    end, "\n")
   end
 
   def person_to_vcard(person)
-    card = Vcard::Vcard::Maker.make2 do |maker|
+    return unless person
 
+    card = Vcard::Vcard::Maker.make2 do |maker|
       maker.add_name do |name|
-        name.prefix = ''
         name.given = person.firstname.to_s
         name.family = person.lastname.to_s
         name.additional = person.middlename.to_s
@@ -28,25 +30,24 @@ module PeopleHelper
 
       maker.add_addr do |addr|
         addr.preferred = true
-        addr.street = person.address.to_s.gsub("\r\n"," ").gsub("\n"," ")
+        addr.street = person.address.to_s.gsub("\r\n", ' ').gsub("\n", ' ')
       end
 
       maker.title = person.job_title.to_s
       maker.org = person.company.to_s
-      maker.birthday = person.birthday.to_date unless person.birthday.blank?
-      maker.add_note(person.background.to_s.gsub("\r\n"," ").gsub("\n", ' '))
-
-      # maker.add_url(person.website.to_s)
+      maker.birthday = person.birthday.to_date if person.birthday.present?
+      maker.add_note(person.background.to_s.gsub("\r\n", ' ').gsub("\n", ' '))
 
       person.phones.each { |phone| maker.add_tel(phone) }
-
-      maker.add_email(person.email)
+      maker.add_email(person.email) if person.email.present?
     end
-    avatar = person.avatar
-    card = card.encode.sub("END:VCARD", "PHOTO;BASE64:" + "\n " + [File.open(avatar.diskfile).read].pack('m').to_s.gsub(/[ \n]/, '').scan(/.{1,76}/).join("\n ") + "\nEND:VCARD") if avatar && avatar.readable?
+
+    if person.avatar&.readable?
+      avatar_data = Base64.strict_encode64(File.binread(person.avatar.diskfile))
+      photo_vcard = "PHOTO;BASE64:\n " + avatar_data.scan(/.{1,76}/).join("\n ") + "\nEND:VCARD"
+      card = card.encode.sub("END:VCARD", photo_vcard)
+    end
 
     card.to_s
-
   end
-
 end
