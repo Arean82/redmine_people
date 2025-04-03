19f4e36d2d339c5e266284e423edb9a082a951d7
arean82 <38061300+Arean82@users.noreply.github.com>
2025-04-03 12:24:10 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@
 # See: http://guides.rubyonrails.org/routing.html
 
 resources :people do
-    collection do
-      get :bulk_edit, :context_menu, :edit_mails, :preview_email, :avatar
-      post :bulk_edit, :bulk_update, :send_mails
-      delete :bulk_destroy
-    end
+  collection do
+    get :bulk_edit, :context_menu, :edit_mails, :preview_email
+    post :bulk_edit, :bulk_update, :send_mails
+    delete :bulk_destroy
+  end
 end
 
 resources :departments do
@@ -17,10 +17,20 @@ resources :departments do
   end
 end
 
-resources :people_settings do
+resources :people_settings, only: [:index, :update, :destroy] do
   collection do
     get :autocomplete_for_user
   end
-end  
-# match "people_settings/:action" => "people_settings"
+end
 
+# API Routes (Versioned)
+namespace :api do
+  namespace :v1 do
+    resources :people, only: [:index, :show, :create, :update, :destroy]
+    resources :departments, only: [:index, :show, :create, :update, :destroy]
+    resources :people_settings, only: [:index, :update]
+
+    post 'people/:id/send_mail', to: 'people#send_mail'
+    get 'departments/:id/people', to: 'departments#people'
+  end
+end
