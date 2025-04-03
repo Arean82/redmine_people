ece94d6cd193e81505f2b78c275b4cdb341fb332
arean82 <38061300+Arean82@users.noreply.github.com>
2025-04-03 12:21:38 +0530

    Update department.rb

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,29 +1,25 @@
 class Department < ActiveRecord::Base
   include Redmine::SafeAttributes
-  unloadable
-  belongs_to :head, :class_name => 'Person', :foreign_key => 'head_id'    
-  has_many :people, -> { uniq }, :dependent => :nullify
 
-  acts_as_nested_set :order => 'name', :dependent => :destroy
+  belongs_to :head, class_name: 'Person', foreign_key: 'head_id'
+  has_many :people, -> { distinct }, dependent: :nullify
+
+  acts_as_nested_set order: 'name', dependent: :destroy
   acts_as_attachable_global
 
-  validates_presence_of :name 
-  validates_uniqueness_of :name 
+  validates :name, presence: true, uniqueness: true
 
-  safe_attributes 'name',
-    'background',
-    'parent_id',
-    'head_id'
+  safe_attributes 'name', 'background', 'parent_id', 'head_id'
 
   def to_s
     name
   end
 
   # Yields the given block for each department with its level in the tree
-  def self.department_tree(departments, &block)
+  def self.department_tree(departments)
     ancestors = []
     departments.sort_by(&:lft).each do |department|
-      while (ancestors.any? && !department.is_descendant_of?(ancestors.last))
+      while ancestors.any? && !department.is_descendant_of?(ancestors.last)
         ancestors.pop
       end
       yield department, ancestors.size
@@ -32,22 +28,19 @@ class Department < ActiveRecord::Base
   end  
 
   def css_classes
-    s = 'project'
-    s << ' root' if root?
-    s << ' child' if child?
-    s << (leaf? ? ' leaf' : ' parent')
-    s
+    classes = ['project']
+    classes << 'root' if root?
+    classes << 'child' if child?
+    classes << (leaf? ? 'leaf' : 'parent')
+    classes.join(' ')
   end
 
   def allowed_parents
-    return @allowed_parents if @allowed_parents
-    @allowed_parents = Department.all
-    @allowed_parents = @allowed_parents - self_and_descendants
-    @allowed_parents << nil
-    unless parent.nil? || @allowed_parents.empty? || @allowed_parents.include?(parent)
-      @allowed_parents << parent
+    @allowed_parents ||= begin
+      all_departments = Department.where.not(id: self_and_descendants.ids)
+      all_departments += [parent] if parent && !all_departments.include?(parent)
+      all_departments << nil
+      all_departments
     end
-    @allowed_parents
   end  
-
 end
